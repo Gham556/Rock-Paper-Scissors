03f622ee4654dfbce17bbff1f892b48d811b72f7
George <georgea.fotheringham@gmail.com>
2022-07-22 15:48:56 -0400

    Create functions that take player input, generate random computer input, and compares to return a message in console
    Issue is output of computerSelection and getComputerChoice are different even though should be equal. Code still works but am workshopping solutions.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function getComputerChoice () {
-    
+const playerSelection = prompt("Enter rock, paper, or scissors");
+
+
+function getComputerChoice(){
+    var choices = ["rock", "paper", "scissors"];
+    const computerSelection = choices[Math.floor(Math.random()*choices.length)];
+    return(computerSelection);
 }
+
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+        if (playerSelection === "rock" && computerSelection === "scissors")
+            return("You win!")
+        else if (playerSelection === "rock" && computerSelection === "rock")
+        return("It's a tie!")
+        else 
+        return("You lose!");
+           
+
+            
+    }
+
+  
+   
+  
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(getComputerChoice());
+  console.log(playerSelection);
+  console.log(computerSelection);
