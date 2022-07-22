f450b9e0cf49d63147f0befa659d1a285231071b
George <georgea.fotheringham@gmail.com>
2022-07-22 17:44:22 -0400

    nested all functions

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
 let playerSelection = "";
 
 
-function getComputerChoice(){
-    var choices = ["rock", "paper", "scissors"];
-    const result = choices[Math.floor(Math.random()*choices.length)];
-    return(result);
-}
-
-const computerSelection = getComputerChoice();
 let playerScore = 0
 let computerScore = 0
 
-function playRound(playerSelection, computerSelection,) {
-    playerSelection += window.prompt("Enter rock, paper, or scissors")  
-    playerSelection.toLowerCase 
-        if (playerSelection === "rock" && computerSelection === "scissors")    
-        return("You win!")
-        else if (playerSelection === "rock" && computerSelection === "rock")
-            return("It's a tie!")
-        else if(playerSelection == "paper" && computerSelection == "rock")
-            return("You win!")
-            
-        else if (playerSelection == "paper" && computerSelection == "paper")
-            return("It's a tie!")
-            
-        else if (playerSelection == "scissors" && computerSelection == "paper")
-            return("You Win!")
-            
-        else if (playerSelection == "scissors" && computerSelection == "scissors")
-            return("It's a tie!")
-        else    
-        return("You lose!");
-            
-            
-    }
-
     function game() {
+        const computerSelection = getComputerChoice();
+        function getComputerChoice(){
+            var choices = ["rock", "paper", "scissors"];
+            const result = choices[Math.floor(Math.random()*choices.length)];
+            return(result);
+        }        
+        function playRound(playerSelection, computerSelection) {
+            playerSelection += window.prompt("Enter rock, paper, or scissors")  
+            playerSelection.toLowerCase 
+                if (playerSelection === "rock" && computerSelection === "scissors")    
+                return("You win!")
+                else if (playerSelection === "rock" && computerSelection === "rock")
+                    return("It's a tie!")
+                else if(playerSelection == "paper" && computerSelection == "rock")
+                    return("You win!")
+                    
+                else if (playerSelection == "paper" && computerSelection == "paper")
+                    return("It's a tie!")
+                    
+                else if (playerSelection == "scissors" && computerSelection == "paper")
+                    return("You Win!")
+                    
+                else if (playerSelection == "scissors" && computerSelection == "scissors")
+                    return("It's a tie!")
+                else    
+                return("You lose!");}
+        function scoreTracker() {
+                    if (playerScore = 5)
+                    console.log("That's the game! You win 5 " + computerScore)
+                else if (computerScore = 5)
+                    console.log("That's the game! You lose 5 " + playerScore)
+                 
+                }
+        
         for (let i = 0; i < 5; i++)
+            getComputerChoice();
             playRound(playerSelection, computerSelection);
             scoreTracker();
         if (playRound = "You win!")
@@ -49,15 +53,11 @@ function playRound(playerSelection, computerSelection,) {
        
     }
 
-    function scoreTracker() {
-        if (playerScore = 5)
-        return("That's the game! You win 5 " + computerScore)
-    else if (computerScore = 5)
-        return("That's the game! You lose 5 " + playerScore)
-     
-    }
+   
   
     game();
+
+
     
    
   
