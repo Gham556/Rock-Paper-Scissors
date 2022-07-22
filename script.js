1748b2cace32c0034e8f47787c1fd846a83519c8
George <georgea.fotheringham@gmail.com>
2022-07-22 17:32:50 -0400

    Add game and round tracker funtions
    Game function runs playRound() 5 times, and calls scoreTracker. Score tracker tracks score from each round and prints message when score reaches 5.
    
    playRound() runs successfully 5 times. Error message says playRound() is not a function is an error. .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const playerSelection = prompt("Enter rock, paper, or scissors");
-playerSelection.toLowerCase
+let playerSelection = "";
+
 
 function getComputerChoice(){
     var choices = ["rock", "paper", "scissors"];
@@ -8,28 +8,59 @@ function getComputerChoice(){
 }
 
 const computerSelection = getComputerChoice();
+let playerScore = 0
+let computerScore = 0
 
-function playRound(playerSelection, computerSelection) {
-        if (playerSelection === "rock" && computerSelection === "scissors")
-            return("You win!")
+function playRound(playerSelection, computerSelection,) {
+    playerSelection += window.prompt("Enter rock, paper, or scissors")  
+    playerSelection.toLowerCase 
+        if (playerSelection === "rock" && computerSelection === "scissors")    
+        return("You win!")
         else if (playerSelection === "rock" && computerSelection === "rock")
             return("It's a tie!")
         else if(playerSelection == "paper" && computerSelection == "rock")
             return("You win!")
+            
         else if (playerSelection == "paper" && computerSelection == "paper")
             return("It's a tie!")
+            
         else if (playerSelection == "scissors" && computerSelection == "paper")
             return("You Win!")
+            
         else if (playerSelection == "scissors" && computerSelection == "scissors")
             return("It's a tie!")
         else    
         return("You lose!");
             
+            
     }
 
+    function game() {
+        for (let i = 0; i < 5; i++)
+            playRound(playerSelection, computerSelection);
+            scoreTracker();
+        if (playRound = "You win!")
+        
+            playerScore++
+        else if (playRound() = "You lose!")
+        
+            computerScore++
+        else;
+       
+    }
+
+    function scoreTracker() {
+        if (playerScore = 5)
+        return("That's the game! You win 5 " + computerScore)
+    else if (computerScore = 5)
+        return("That's the game! You lose 5 " + playerScore)
+     
+    }
   
+    game();
+    
    
   
-  console.log(playRound(playerSelection, computerSelection));
-  console.log("You chose " + playerSelection);
+  console.log(game());
   console.log("Opponent chose " + computerSelection);
+  console.log(scoreTracker())
