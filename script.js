ace11ee350ceba64bf0cb7c528407733b1092fe0
George <georgea.fotheringham@gmail.com>
2022-07-23 01:26:23 -0400

    Added getComputerChoice and computer selection to Game() loop
    Loop runs a new instance of getComputerChoice each round and computerSelection picks up the new value, preventing the computer from picking the same option every round.
    
    Found issue with the window storing computerSelection as undefined even though it had a value in script. Fixed by removing computerSelection from variable of playRound.
    .

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 let playerScore = 0
 let computerScore = 0
 
@@ -13,27 +10,30 @@ function getComputerChoice(){
 
 let computerSelection = getComputerChoice();
 
-function playRound(playerSelection, computerSelection) {
+
+
+function playRound() {
             
         playerSelection = prompt("Enter rock, paper, or scissors")
         playerSelection.toLowerCase 
-        let test = playerSelection 
-         
+        
+        
+
   
-      if (test === "rock" && computerSelection === "scissors") {   
+      if (playerSelection === "rock" && computerSelection === "scissors") {   
       return(playerScore++) }
-      else if (test === "rock" && computerSelection === "rock") {
+      else if (playerSelection === "rock" && computerSelection === "rock") {
           ; }
-      else if(test === "paper" && computerSelection === "rock") {
+      else if(playerSelection === "paper" && computerSelection === "rock") {
            return(playerScore++) }
           
-      else if (test === "paper" && computerSelection === "paper") {
+      else if (playerSelection === "paper" && computerSelection === "paper") {
           ; }
           
-      else if (test === "scissors" && computerSelection === "paper") {
+      else if (playerSelection === "scissors" && computerSelection === "paper") {
           return(playerScore++) }
           
-      else if (test === "scissors" && computerSelection === "scissors") {
+      else if (playerSelection === "scissors" && computerSelection === "scissors") {
            ; }
       
       else    {
@@ -44,15 +44,16 @@ function playRound(playerSelection, computerSelection) {
 
         
 function game() {
-    for(i = 0; i < 5; i++)
-    playRound()
-    getComputerChoice()
+    for(i = 0; i < 5; i++) {
+        getComputerChoice()
+        computerSelection = getComputerChoice()
+        playRound()
+    ;}
 }
     
 game()
    
 console.log("Your score is " + playerScore + " and the computer score is " + computerScore)
-console.log(playRound())
-console.log(computerSelection)
+
 
   
