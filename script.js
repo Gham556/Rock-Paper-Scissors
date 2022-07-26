e730fc520ba94fde0c557f21291608f17132f5b2
George <georgea.fotheringham@gmail.com>
2022-07-25 21:44:06 -0400

    function increase score tracker and randomize compChoice every button press

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 //UI//
 
 const buttons = Array.from(document.getElementsByTagName('button'));
+const playerScore = document.getElementById('score')
+const compScore = document.getElementById('compScore')
 
 buttons.forEach(button => button.addEventListener('click', playRound));
 
@@ -20,25 +22,26 @@ function playRound(button) {
           
 
       if (button.target.className === 'rock' && getComputerChoice() === "scissors") {   
-      console.log("You Win!") }
+      console.log("You Win!") 
+      playerScore.textContent++;}
       else if (button.target.className === 'rock' && getComputerChoice() === "rock") {
           console.log("It's A Tie!"); }
       else if(button.target.className === "paper" && getComputerChoice() === "rock") {
-           console.log("You Win!") }
-          
+           console.log("You Win!") 
+           playerScore.textContent++;}
       else if (button.target.className === "paper" && getComputerChoice() === "paper") {
           console.log("It's A Tie"); }
           
       else if (button.target.className === "scissors" && getComputerChoice() === "paper") {
-          console.log("You Win!") }
-          
+          console.log("You Win!") 
+          playerScore.textContent++;
+      }
       else if (button.target.className === "scissors" && getComputerChoice() === "scissors") {
            console.log("It's A Tie!"); }
-      
       else    {
-      console.log("You Lose!") }
+      console.log("You Lose!") 
+        compScore.textContent++;}
 
-   console.log(computerSelection)
   }
 
         
